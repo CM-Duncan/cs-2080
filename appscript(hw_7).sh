22fd8d3e52eb49c43a9684f27cfb638f891a5c86
CM-Duncan <carolineduncan51@gmail.com>
2025-04-10 10:09:47 -0600

    Update appscript(hw_7).sh

--- appscript(hw_7).sh
+++ appscript(hw_7).sh
@@ -6,10 +6,9 @@
 #!/bin/bash
 
 service nginx start
-
+# Use sed to replace: “Welcome to nginx” with “Welcome to <YOUR_FIRST_NAME>’s page” 
 sed -i "s/Welcome to nginx/Welcome to Caroline’s page/" /var/www/html/index.nginx-debian.html
+#Use sed to replace all occurrences of “nginx” in the same html file with “nginx (pronounced as EngineX)”
 sed -i "s/nginx/nginx (pronounced as EngineX)/g" /var/www/html/index.nginx-debian.html
 
 service nginx restart
-
-tail -f /dev/null
